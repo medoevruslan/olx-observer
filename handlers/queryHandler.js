086154c36eb7bb686bf80d398cc3c6ed2bba2ecc
MedoevRuslan <mr.r.m3@icloud.com>
2022-08-05 15:47:59 +0300

    try catch in scrapper

--- handlers/queryHandler.js
+++ handlers/queryHandler.js
@@ -10,33 +10,31 @@ const { Card } = require('../models/card');
 const { fetchRegexFromQuery, filterByRegex } = require('../handlers/regexHandler');
 const { getQueriesFromDb } = require('../controller/queryController');
 const QueryDto = require('../dtos/QueryDto');
- 
+
 async function scrapByQuery(query) {
-    try {
-        const scrapper = new Scrapper(query);
-        let result = [];
-        result = await scrapper.scrap();
-        const flatted = result.flat();
-        return flatted;
-    } catch(err) {
-        console.error('SCRAPPING ERROR ---- ' + err);
-        return [];
-    }
+    const scrapper = new Scrapper(query);
+    let result = [];
+    result = await scrapper.scrap();
+    const flatted = result.flat();
+    return flatted;
 }
 
 async function processQueryToDb(query) {
     let isObserved = false;
-    let user = await User.findOne({ where : { chatId: query.chatId } });
+    let user = await User.findOne({ where: { chatId: query.chatId } });
     const assignedQuery = queryBuilder(query)
     if (user === null) {
         user = await User.create({ chatId: query.chatId, userName: query.userName });
         await user.createQuery(assignedQuery);
     } else {
         let srchQry = await user.getQueries(
-           { where: {
-                searchQuery: sequelize.where(
-                    sequelize.fn('LOWER', sequelize.col('searchQuery')), 
-                        'LIKE', `%${assignedQuery.searchQuery.toLowerCase()}%`) } });
+            {
+                where: {
+                    searchQuery: sequelize.where(
+                        sequelize.fn('LOWER', sequelize.col('searchQuery')),
+                        'LIKE', `%${assignedQuery.searchQuery.toLowerCase()}%`)
+                }
+            });
         if (srchQry.length === 0) {
             await user.createQuery(assignedQuery);
         } else {
@@ -55,34 +53,34 @@ async function addCardsToDb() {
     const queries = await getQueriesDto();
     queries.forEach(async (query) => {
         const { category, searchQuery, regexForModel, queryId, maxPrice } = query;
-        const data = await scrapByQuery({category, searchQuery, queryId});
+        const data = await scrapByQuery({ category, searchQuery, queryId });
         const regex = fetchRegexFromQuery(query)
         let afterRegex = filterByRegex({ regex, data }, regexForModel)
         const benefitPrices = getBenefitPrice(afterRegex, maxPrice);
         const dateConvereted = dateToTimestamp(benefitPrices);
-        // await saveCardsToDb(dateConvereted);
+        await saveCardsToDb(dateConvereted);
         getLog({ data, regex, afterRegex, dateConvereted, query });
     })
 }
 
 async function saveCardsToDb(cards) {
-    await Card.bulkCreate(cards, {ignoreDuplicates: true});
+    await Card.bulkCreate(cards, { ignoreDuplicates: true });
 }
 
 function queryBuilder(query) {
     const { maxPrice, regexModelTxt, regexForModel, brand, model, regex, category } = query;
     const searchFor = `q-${brand}-${model.replace(/\s/g, '-')}`;
-    return { 
-        searchQuery: searchFor, 
-        category: category, 
-        regex: `${regex}`, 
-        maxPrice: maxPrice, 
+    return {
+        searchQuery: searchFor,
+        category: category,
+        regex: `${regex}`,
+        maxPrice: maxPrice,
         regexModelTxt: regexModelTxt,
         regexForModel: regexForModel
-     }
+    }
 }
 
-function getLog({data, regex, afterRegex, dateConvereted, query}) {
+function getLog({ data, regex, afterRegex, dateConvereted, query }) {
     console.log(new Date(Date.now()));
     console.log(regex.brand, regex.model, query.searchQuery);
     console.log(`before regex - ${data.length}`);
--- handlers/scrapper.js
+++ handlers/scrapper.js
@@ -12,45 +12,51 @@ class Scrapper {
     }
 
     async scrap() {
-        const browser = await puppeteer.launch({headless: true, timeout: 0});
-        const page = await browser.newPage();
-        await page.goto(this._URL_CATEGORY.href, {waitUntil: 'load'});
-
-        const cardsArray =[];
-        let firstPage = true;
-        
-        while (true){
-            const forward = await page.$('.pagination-list a[data-testid=pagination-forward]');
-            if (!forward && !firstPage) break;
-            const cards = await page.evaluate((queryId) => {
-                return [...document.querySelectorAll('div[data-cy=l-card]')]
-                    .map(el => {  
-                        return {
-                            name: el.querySelector('h6').textContent,
-                            price: el.querySelector('h6').nextElementSibling.textContent,
-                            link: 'olx.ua' + el.querySelector('a').getAttribute('href'),
-                            time: el.querySelector('p[data-testid=location-date]').textContent.split(' - ')[1],
-                            queryId: queryId,
-                        }
-                    });
-            }, this._queryId);
-            
-            cardsArray.push(cards);
-            
-            if (forward) {
-                await Promise.all([
-                    await page.click('.pagination-list a[data-testid=pagination-forward]'),
-                    await page.waitForSelector('.pagination-list', {timeout: 0}),
-                ])
-            }
-
-            if (firstPage) firstPage = false;
-        }
+        let browser = null;
+        try {
+            browser = await puppeteer.launch({headless: true, timeout: 0});
+            const page = await browser.newPage();
+            await page.goto(this._URL_CATEGORY.href, {waitUntil: 'load'});
     
-        await page.close();
-        await browser.close();
+            const cardsArray =[];
+            let firstPage = true;
+            
+            while (true){
+                const forward = await page.$('.pagination-list a[data-testid=pagination-forward]');
+                if (!forward && !firstPage) break;
+                const cards = await page.evaluate((queryId) => {
+                    return [...document.querySelectorAll('div[data-cy=l-card]')]
+                        .map(el => {  
+                            return {
+                                name: el.querySelector('h6').textContent,
+                                price: el.querySelector('h6').nextElementSibling.textContent,
+                                link: 'olx.ua' + el.querySelector('a').getAttribute('href'),
+                                time: el.querySelector('p[data-testid=location-date]').textContent.split(' - ')[1],
+                                queryId: queryId,
+                            }
+                        });
+                }, this._queryId);
+                
+                cardsArray.push(cards);
+                
+                if (forward) {
+                    await Promise.all([
+                        await page.click('.pagination-list a[data-testid=pagination-forward]'),
+                        await page.waitForSelector('.pagination-list', {timeout: 0}),
+                    ])
+                }
+    
+                if (firstPage) firstPage = false;
+            }
         
-        return cardsArray;
+            await page.close();
+            await browser.close();
+            
+            return cardsArray;
+        } catch (err) {
+            console.error('SCRAPPING ERROR ---- ' + err);
+            await browser.close();
+        }
     }
 }
 
