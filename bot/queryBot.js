55bb8b7babf7e1c0c768409fd0c1c0514d5c0075
MedoevRuslan <mr.r.m3@icloud.com>
2025-11-07 18:28:00 +0200

    fix(scrapper): fix querySelector query

--- bot/queryBot.js
+++ bot/queryBot.js
@@ -1,11 +1,12 @@
-'use strict'
+"use strict";
 
-require('dotenv').config();
-const { Telegraf, Markup, session, Scenes } = require('telegraf');
-const searchWizard = require('./scenes/search');
-const optionsWizard = require('./scenes/options');
-const sequelize  = require('../db.sequelize');
-const { startBtn } = require('./buttons/botContent');
+require("dotenv").config();
+
+const { Telegraf, Markup, session, Scenes } = require("telegraf");
+const searchWizard = require("./scenes/search");
+const optionsWizard = require("./scenes/options");
+const sequelize = require("../db.sequelize");
+const { startBtn } = require("./buttons/botContent");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -13,23 +14,25 @@ const stage = new Scenes.Stage([searchWizard, optionsWizard]);
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.hears(startBtn.search, ctx => ctx.scene.enter('searchWizard'));
-bot.hears(startBtn.options, ctx => ctx.scene.enter('optionsScene'));
+bot.hears(startBtn.search, (ctx) => ctx.scene.enter("searchWizard"));
+bot.hears(startBtn.options, (ctx) => ctx.scene.enter("optionsScene"));
 
 bot.start(async (ctx) => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        await ctx.reply('С чего начнем?', Markup.keyboard([
-            [startBtn.search],
-            [startBtn.options]
-        ]).resize().oneTime())
-    } catch (err) {
-        console.log(err);
-    }
-})
-  
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    await ctx.reply(
+      "С чего начнем?",
+      Markup.keyboard([[startBtn.search], [startBtn.options]])
+        .resize()
+        .oneTime()
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
 
 module.exports = bot;
--- handlers/scrapper.js
+++ handlers/scrapper.js
@@ -1,135 +1,157 @@
-'use strict';
+"use strict";
 
-const { Cluster } = require('puppeteer-cluster');
+const { Cluster } = require("puppeteer-cluster");
 
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 class _Scrapper {
+  constructor({
+    rootUrl = "https://www.olx.ua/",
+    category = "",
+    searchQuery = "",
+    queryId,
+  }) {
+    this._URL = new URL(rootUrl);
+    this._URL_CATEGORY = new URL(category + searchQuery, this._URL);
+    this._URL_CATEGORY.searchParams.set("currency", "UAH");
+    this._queryId = queryId;
+  }
 
-    constructor({rootUrl = 'https://www.olx.ua/', category = '', searchQuery = '', queryId}) {
-        this._URL = new URL(rootUrl);
-        this._URL_CATEGORY = new URL(category + searchQuery, this._URL);
-        this._URL_CATEGORY.searchParams.set('currency', 'UAH');
-        this._queryId = queryId;
-    }
+  async scrap() {
+    console.error("START SCRAPPING ---- ");
+    let browser = null;
+    try {
+      browser = await puppeteer.launch({ headless: true, timeout: 0 });
+      const page = await browser.newPage();
+      await page.goto(this._URL_CATEGORY.href, { waitUntil: "load" });
 
-    async scrap() {
-        console.error('START SCRAPPING ---- ');
-        let browser = null;
-        try {
-            browser = await puppeteer.launch({headless: true, timeout: 0});
-            const page = await browser.newPage();
-            await page.goto(this._URL_CATEGORY.href, {waitUntil: 'load'});
-    
-            const cardsArray =[];
-            let firstPage = true;
-            
-            while (true){
-                const forward = await page.$('.pagination-list a[data-testid=pagination-forward]');
-                if (!forward && !firstPage) break;
-                const cards = await page.evaluate((queryId) => {
-                    return [...document.querySelectorAll('div[data-cy=l-card]')]
-                        .map(el => {  
-                            return {
-                                name: el.querySelector('h6').textContent,
-                                price: el.querySelector('h6').nextElementSibling.textContent,
-                                link: 'olx.ua' + el.querySelector('a').getAttribute('href'),
-                                time: el.querySelector('p[data-testid=location-date]').textContent.split(' - ')[1],
-                                queryId: queryId,
-                            }
-                        });
-                }, this._queryId);
+      const cardsArray = [];
+      let firstPage = true;
 
-                cardsArray.push(cards);
-                
-                if (forward) {
-                    await Promise.all([
-                        await page.click('.pagination-list a[data-testid=pagination-forward]'),
-                        await page.waitForSelector('.pagination-list', {timeout: 0}),
-                    ])
-                }
-    
-                if (firstPage) firstPage = false;
+      while (true) {
+        const forward = await page.$(
+          ".pagination-list a[data-testid=pagination-forward]"
+        );
+        if (!forward && !firstPage) break;
+        const cards = await page.evaluate((queryId) => {
+          return [...document.querySelectorAll("div[data-cy=l-card]")].map(
+            (el) => {
+              return {
+                name: el.querySelector("h6").textContent,
+                price: el.querySelector("h6").nextElementSibling.textContent,
+                link: "olx.ua" + el.querySelector("a").getAttribute("href"),
+                time: el
+                  .querySelector("p[data-testid=location-date]")
+                  .textContent.split(" - ")[1],
+                queryId: queryId,
+              };
             }
-        
-            await page.close();
-            await browser.close();
-            
-            return cardsArray;
-        } catch (err) {
-            console.error('SCRAPPING ERROR ---- ' + err);
-            await browser.close();
+          );
+        }, this._queryId);
+
+        cardsArray.push(cards);
+
+        if (forward) {
+          await Promise.all([
+            await page.click(
+              ".pagination-list a[data-testid=pagination-forward]"
+            ),
+            await page.waitForSelector(".pagination-list", { timeout: 0 }),
+          ]);
         }
+
+        if (firstPage) firstPage = false;
+      }
+
+      await page.close();
+      await browser.close();
+
+      return cardsArray;
+    } catch (err) {
+      console.error("SCRAPPING ERROR ---- " + err);
+      await browser.close();
     }
+  }
 }
 
 class Scrapper {
-    constructor({ rootUrl = 'https://www.olx.ua/', category = '', searchQuery = '', queryId }) {
-        this._URL = new URL(rootUrl);
-        this._URL_CATEGORY = new URL(category + searchQuery, this._URL);
-        this._URL_CATEGORY.searchParams.set('currency', 'UAH');
-        this._queryId = queryId;
-    }
+  constructor({
+    rootUrl = "https://www.olx.ua/",
+    category = "",
+    searchQuery = "",
+    queryId,
+  }) {
+    this._URL = new URL(rootUrl);
+    this._URL_CATEGORY = new URL(category + searchQuery, this._URL);
+    this._URL_CATEGORY.searchParams.set("currency", "UAH");
+    this._queryId = queryId;
+  }
 
-    async scrap() {
-        console.error('START SCRAPPING ---- ');
-        let cardsArray = [];
-        const cluster = await Cluster.launch({
-            concurrency: Cluster.CONCURRENCY_PAGE,
-            maxConcurrency: 10, // Adjust the number of concurrent instances as needed
-            puppeteerOptions: {
-                headless: true,
-                timeout: 0,
-                args: ['--no-sandbox', '--disable-gpu']
+  async scrap() {
+    console.error("START SCRAPPING ---- ");
+    let cardsArray = [];
+    const cluster = await Cluster.launch({
+      concurrency: Cluster.CONCURRENCY_PAGE,
+      maxConcurrency: 10, // Adjust the number of concurrent instances as needed
+      puppeteerOptions: {
+        headless: true,
+        timeout: 0,
+        args: ["--no-sandbox", "--disable-gpu"],
+      },
+    });
+
+    await cluster.task(async ({ page, data: queryId }) => {
+      await page.goto(this._URL_CATEGORY.href, { waitUntil: "load" });
+
+      let firstPage = true;
+
+      while (true) {
+        const forward = await page.$(
+          ".pagination-list a[data-testid=pagination-forward]"
+        );
+        if (!forward && !firstPage) break;
+
+        const cards = await page.evaluate((queryId) => {
+          return [...document.querySelectorAll("div[data-cy=l-card]")].map(
+            (el) => {
+              return {
+                name: el.querySelector("h4")?.textContent ?? "wrongSelector",
+                price:
+                  el.querySelector("p[data-testid=ad-price]")?.textContent ??
+                  "wrongSelector",
+                link: "olx.ua" + el.querySelector("a").getAttribute("href"),
+                time: el
+                  .querySelector("p[data-testid=location-date]")
+                  .textContent.split(" - ")[1],
+                queryId: queryId,
+              };
             }
+          );
         });
 
-        await cluster.task(async ({ page, data: queryId }) => {
-            await page.goto(this._URL_CATEGORY.href, { waitUntil: 'load' });
+        cardsArray.push(cards);
 
-            let firstPage = true;
+        if (forward) {
+          await Promise.all([
+            page.click(".pagination-list a[data-testid=pagination-forward]"),
+            page.waitForSelector(".pagination-list", { timeout: 0 }),
+          ]);
+        }
 
-            while (true) {
-                const forward = await page.$('.pagination-list a[data-testid=pagination-forward]');
-                if (!forward && !firstPage) break;
+        if (firstPage) firstPage = false;
+      }
+    });
 
-                const cards = await page.evaluate((queryId) => {
-                    return [...document.querySelectorAll('div[data-cy=l-card]')]
-                        .map(el => {
-                            return {
-                                name: el.querySelector('h6').textContent,
-                                price: el.querySelector('h6').nextElementSibling.textContent,
-                                link: 'olx.ua' + el.querySelector('a').getAttribute('href'),
-                                time: el.querySelector('p[data-testid=location-date]').textContent.split(' - ')[1],
-                                queryId: queryId,
-                            }
-                        });
-                });
+    await cluster.execute(this._queryId);
 
-                cardsArray.push(cards);
+    await cluster.idle();
+    await cluster.close();
 
-                if (forward) {
-                    await Promise.all([
-                        page.click('.pagination-list a[data-testid=pagination-forward]'),
-                        page.waitForSelector('.pagination-list', { timeout: 0 }),
-                    ]);
-                }
-
-                if (firstPage) firstPage = false;
-            }
-        });
-
-        await cluster.execute(this._queryId);
-
-        await cluster.idle();
-        await cluster.close();
-
-        console.error('SCRAPPING COMPLETED ---- ');
-        return cardsArray;
-    }
+    console.error("SCRAPPING COMPLETED ---- ");
+    return cardsArray;
+  }
 }
 
 module.exports = { Scrapper };
-
 
 module.exports = { Scrapper };
